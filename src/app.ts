8adb7302f11be3015e659f9fc08672addd3686b7
akhildas675 <akhildas675@gmail.com>
2025-11-28 10:56:24 +0530

    initial backend functionality test with sample code

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/test", (req, res) => {
+  res.json({
+    success: true,
+    message: "Backend is working!"
+  });
+});
+
+export default app;
