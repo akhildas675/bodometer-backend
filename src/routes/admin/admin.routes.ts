c9a44763e5dd142b74960a9a64076e4fa20a9563
akhildas675 <akhildas675@gmail.com>
2026-01-27 12:55:58 +0530

    fix(admin-trainer):WIP admin trainer onboarding routes fixed

--- src/routes/admin/admin.routes.ts
+++ src/routes/admin/admin.routes.ts
@@ -6,62 +6,90 @@ import { S3Service } from "../../services/s3/s3.service";
 import { authGuard } from "../../middleware/authGuard";
 import { imageUpload } from "../../config/multer";
 
-
 const adminRoute = Router();
 const adminRepository = new AdminRepository();
-const s3Service = new S3Service()
+const s3Service = new S3Service();
 const adminService = new AdminService(adminRepository, s3Service);
 const adminController = new AdminController(adminService);
 
-
-
+// User Management
 adminRoute.get(
   "/admin/get-users",
   authGuard(["admin"]),
-  adminController.getUsers,
+  adminController.getUsers
 );
 
 adminRoute.patch(
   "/admin/users/:userId/block",
   authGuard(["admin"]),
-  adminController.blockUser,
+  adminController.blockUser
 );
 
 adminRoute.patch(
   "/admin/users/:userId/unblock",
   authGuard(["admin"]),
-  adminController.unblockUser,
+  adminController.unblockUser
 );
 
+// Trainer Management (Block/Unblock)
 adminRoute.get(
   "/admin/get-trainers",
   authGuard(["admin"]),
-  adminController.getTrainers,
+  adminController.getTrainers
 );
+
 adminRoute.patch(
   "/admin/trainer/:trainerId/block",
   authGuard(["admin"]),
-  adminController.blockTrainer,
+  adminController.blockTrainer
 );
 
 adminRoute.patch(
   "/admin/trainer/:trainerId/unblock",
   authGuard(["admin"]),
-  adminController.unblockTrainer,
+  adminController.unblockTrainer
 );
+
+// workout management
 adminRoute.post(
   "/admin/add-workout",
   authGuard(["admin"]),
-  imageUpload.single("workoutImage"), adminController.addWorkout
+  imageUpload.single("workoutImage"),
+  adminController.addWorkout
 );
+
 adminRoute.get(
   "/admin/get-workouts",
-  authGuard(["admin"]), adminController.getWorkout
-)
+  authGuard(["admin"]),
+  adminController.getWorkout
+);
 
-adminRoute.post("/get-trainer-appointments", authGuard(["admin"]), adminController.getTrainerAppointments);
-adminRoute.get("/trainers/:userId", authGuard(["admin"]), adminController.getTrainerById);
-adminRoute.patch("/trainers/:profileId/approve", authGuard(["admin"]), adminController.approveTrainer);
-adminRoute.patch("/trainers/:profileId/reject", authGuard(["admin"]), adminController.rejectTrainer);
+//trainer appointment management 
+adminRoute.post(
+  "/admin/get-trainer-appointments",
+  authGuard(["admin"]),
+  adminController.getTrainerAppointments
+);
+
+//get trainer by profileId
+adminRoute.get(
+  "/admin/trainers/profile/:profileId",
+  authGuard(["admin"]),
+  adminController.getTrainerById
+);
+
+//Approve trainer
+adminRoute.patch(
+  "/admin/trainers/:profileId/approve",
+  authGuard(["admin"]),
+  adminController.approveTrainer
+);
+
+//Reject trainer
+adminRoute.patch(
+  "/admin/trainers/:profileId/reject",
+  authGuard(["admin"]),
+  adminController.rejectTrainer
+);
 
 export default adminRoute;
