5326c655bc8449ceb9305204d9ea050dda9e3fa7
akhildas675 <akhildas675@gmail.com>
2026-05-26 23:00:11 +0530

    fix: remove dist folder

--- dist/constants/verification.constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.VERIFICATION_STATUS = void 0;
-exports.VERIFICATION_STATUS = {
-    PENDING: "pending",
-    APPROVED: "approved",
-    REJECTED: "rejected",
-};
